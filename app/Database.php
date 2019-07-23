dc951eddb0c2913184d12341513689922aff6391
Jack Marchant <jack@jackmarchant.com>
2019-07-23 21:10:46 +1000

    graphql api works

--- app/Database.php
+++ app/Database.php
@@ -12,7 +12,7 @@ class Database
             $conn = new \PDO("mysql:host=$host;dbname=$database;port=3306", $settings['username'], $settings['password']);
             $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             return $conn;
-        } catch(PDOException $e) {
+        } catch(\PDOException $e) {
             \error_log('Connection to database failed: ' . $e->getMessage());
         }
     }
